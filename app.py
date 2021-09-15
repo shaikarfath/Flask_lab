53c098be6a9d5faf1861f7124252f1a779aba1f4
shaikarfath <shaik_arfath@columbusstate.edu>
2021-09-15 16:29:26 +0000

    commit

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
-if __name__ == "__main__":
-    app.run(debug=True) 
 
 @app.route("/<name>")
 def hello_name(name):
@@ -16,3 +14,5 @@ def about():
     name = request.args.get('name') if request.args.get('name') else "Hello World!"
     return render_template("about.html", aboutName = name)
 
+if __name__ == "__main__":
+    app.run(debug=True) 
